3804b1a771ee5734f4d54978ecb5ae400d6807c5
chrys-unito <chrysc@unito.io>
2022-01-11 00:26:22 -0500

    Push latest changes

--- public/script.js
+++ public/script.js
@@ -1,9 +1,8 @@
 const MAX_IMAGE_COUNT = 3;
+const MIN_ALLOWED_SIZE_PXLS = 500;
 let rotationAngle = 0;
 const randomCoreIndex = getRandomInt(MAX_IMAGE_COUNT);
 const randomOuterRimIndex = getRandomInt(MAX_IMAGE_COUNT);
-console.log('randomCoreIndex: ', randomCoreIndex);
-console.log('randomOuterRimIndex: ', randomOuterRimIndex);
 let currentCore;
 let currentOuterRim;
 let canvas;
@@ -19,16 +18,16 @@ function preload() {
 
 function resizeImage() {
     if (currentCore && currentOuterRim) {
-        const imageScreenWidthRatio = (currentCore.width / Math.max(window.innerWidth, 500));
-        const imageScreenHeightRatio = (currentCore.height / Math.max(window.innerHeight, 500));
+        const imageScreenWidthRatio = (currentCore.width / Math.max(window.innerWidth, MIN_ALLOWED_SIZE_PXLS));
+        const imageScreenHeightRatio = (currentCore.height / Math.max(window.innerHeight, MIN_ALLOWED_SIZE_PXLS));
         const newImageWidth = currentCore.width / Math.min(imageScreenWidthRatio, imageScreenHeightRatio);
         const newImageHeight = currentCore.height / Math.min(imageScreenWidthRatio, imageScreenHeightRatio);
-        const outerRimScreenWidthRatio = (currentOuterRim.width / window.innerWidth);
-        const outerRimScreenHeightRatio = (currentOuterRim.height / window.innerHeight);
+        const outerRimScreenWidthRatio = (currentOuterRim.width / Math.max(window.innerWidth, MIN_ALLOWED_SIZE_PXLS));
+        const outerRimScreenHeightRatio = (currentOuterRim.height / Math.max(window.innerHeight, MIN_ALLOWED_SIZE_PXLS));
         const newOuterRimWidth = currentOuterRim.width / Math.min(outerRimScreenWidthRatio, outerRimScreenHeightRatio);
         const newOuterRimHeight = currentOuterRim.height / Math.min(outerRimScreenWidthRatio, outerRimScreenHeightRatio);
-        currentCore.resize(newImageWidth, newImageHeight);
-        currentOuterRim.resize(newOuterRimWidth, newOuterRimHeight);
+        currentCore.resize(newImageWidth * 1.25, newImageHeight * 1.25);
+        currentOuterRim.resize(newOuterRimWidth * 1.25, newOuterRimHeight * 1.25);
         redraw();
     }
 }
@@ -48,8 +47,8 @@ function setup(){
 let alternateRotation = false;
 
 function draw(){
-    translate(window.innerWidth / 4, window.innerHeight / 2.5);
     imageMode(CENTER);
+    translate(window.innerWidth / 2, window.innerHeight / 2);
     rotate(rotationAngle);
     image(currentCore, 0, 0,);
     image(currentOuterRim, 0, 0);
